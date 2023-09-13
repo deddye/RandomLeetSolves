3a1807884eaed938de1853fc9ea57397b8e258d5
David Dye <ethandye34@gmail.com>
2023-09-13 12:17:05 -0400

    more

--- solutions.ts
+++ solutions.ts
@@ -1,6 +1,6 @@
 //27. Remove Element
 // function removeElement(nums: number[], val: number): number {
-//     let count = 0;    
+//     let count = 0;
 
 //     nums.forEach(n => {
 //         if( n !== val ) nums[count++] = n
@@ -32,3 +32,72 @@
 
 //     return r;
 // };
+
+//189. Rotate Array Right
+
+//  function rotate(nums: number[], k: number): void {
+//     while(k--) {
+//         let num = nums.pop()
+//         if (typeof num !== 'undefined')
+//             nums.unshift(num)
+//     };
+// };
+
+//55. Jump Game
+// function canJump(nums: number[]): boolean {
+//     let idx = nums[0]
+//     for(let i = 1; i < nums.length; i++) {
+//         if(idx == 0) {
+//             return false
+//         }
+//         idx = Math.max(idx - 1, nums[i])
+//     }
+//     return true
+// };
+
+//125. Valid Palindrome
+
+// function isPalindrome(s: string): boolean {
+
+//     let mid = s.length / 2;
+
+//     let idx = 0;
+//     s = s.toLowerCase();
+
+//     s = s.split(/\W+/).join('');
+//     s = s.split('_').join('');
+
+//     while (idx < mid) {
+//         if ( s[idx] === s[s.length - (idx + 1)]) {
+//             idx++;
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// };
+
+//11. Container with Most Water
+
+function maxArea(height: number[]): number {
+  let left = 0;
+  let right = height.length - 1;
+
+  let max = 0;
+
+  while (left < right) {
+    let area = (right - left) * Math.min(height[left], height[right]);
+    max = Math.max(max, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else if (height[left] > height[right]) {
+      right++;
+    } else {
+      left++;
+    }
+  }
+  return max;
+}
