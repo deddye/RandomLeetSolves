e8f364b42397dedf06941ce6dd4817c422a1aabb
David Dye <ethandye34@gmail.com>
2023-09-13 09:57:51 -0400

    169

--- solutions.ts
+++ solutions.ts
@@ -1,9 +1,34 @@
 //27. Remove Element
-function removeElement(nums: number[], val: number): number {
-    let count = 0;    
+// function removeElement(nums: number[], val: number): number {
+//     let count = 0;    
 
-    nums.forEach(n => {
-        if( n !== val ) nums[count++] = n
-    })
-    return count
-};
+//     nums.forEach(n => {
+//         if( n !== val ) nums[count++] = n
+//     })
+//     return count
+// };
+
+//169. Majority Element
+// function majorityElement(nums: number[]): number {
+
+//     if (nums.length === 1) { return nums[0] };
+
+//     let map = new Map<number, number>();
+//     let r = 0;
+//     nums.forEach(n => {
+//         if (map.has(n)) {
+//             let value: number | undefined = map.get(n)
+//             if(typeof value !== "undefined"){
+//                 if ( value + 1 > nums.length / 2) {
+//                     r = n;
+//                 }
+//                 map.set(n, value + 1)
+//             }
+//         }
+//         else {
+//             map.set(n, 1);
+//         }
+//     })
+
+//     return r;
+// };
