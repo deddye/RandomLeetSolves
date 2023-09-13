f6ea21e8355273ed37ced481da85aa60775095a1
David Dye <ethandye34@gmail.com>
2023-09-13 12:19:44 -0400

    11

--- solutions.ts
+++ solutions.ts
@@ -81,23 +81,18 @@
 
 //11. Container with Most Water
 
-function maxArea(height: number[]): number {
-  let left = 0;
-  let right = height.length - 1;
+// function maxArea(height: number[]): number {
+//   let left = 0;
+//   let right = height.length - 1;
 
-  let max = 0;
+//   let max = 0;
 
-  while (left < right) {
-    let area = (right - left) * Math.min(height[left], height[right]);
-    max = Math.max(max, area);
+//   while (left < right) {
+//     let area = (right - left) * Math.min(height[left], height[right]);
+//     max = Math.max(max, area);
 
-    if (height[left] < height[right]) {
-      left++;
-    } else if (height[left] > height[right]) {
-      right++;
-    } else {
-      left++;
-    }
-  }
-  return max;
-}
+//     if (height[left] < height[right]) left++;
+//     else right--;
+//   }
+//   return max;
+// }
